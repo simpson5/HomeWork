5e49294e7102dfdad38c3a09343c2d597c56b949
simpson <450054@naver.com>
2020-12-16 01:59:18 +0900

    201215_HW

---
+++ 201215_HW_NestedLoop_2DimetionArray/src/com/kh/test/simpson/SnailArray.java
@@ -0,0 +1,117 @@
+package com.kh.test.simpson;
+
+import java.util.Scanner;
+
+public class SnailArray {
+
+	public static void main(String[] args) {
+		SnailArray s = new SnailArray();
+		s.test1();
+
+	}
+	
+	public void test1() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수를 입력하시오.");
+		int x= sc.nextInt();
+		int[][] arr = new int[x][x];
+		int num = 1;
+		arr[0][0]=1;
+		for(int i=0; i<x; i++) {
+			for(int j=0; j<x; j++) {
+				if(i==0) {
+					arr[i][j] = arr[0][0]+j;
+					System.out.print("["+i+"]"+"["+j+"]"+"/."+arr[i][j]); //첫번째 행
+				}
+				num= (4*(i)*x)-((2*i)*(2*i)); //기준점 숫자
+				int k =i-1;
+				if(i!=0 && i<(x-1)) {
+					if(i<=(x/2) ||(i>(x/2) && j<(x-i))){ 
+						if(j==k){
+							arr[i][j]=num;
+							System.out.print("["+i+"]"+"["+j+"]"+"/"+arr[i][j]); 
+							//기준점 숫자 생성
+						}
+						else if(j>=i && j<(x-i)) {
+							arr[i][j]=arr[i][j-1]+1;
+							System.out.print("["+i+"]"+"["+j+"]"+"/"+arr[i][j]); 
+							//왼쪽 열보다 1증가
+						}
+						else if(j>=(x-i)) {
+							arr[i][j]=arr[i-1][j]+1;
+							System.out.print("["+i+"]"+"["+j+"]"+"/"+arr[i][j]); 
+							//위쪽 행보다 1증가
+						}
+						else if(j<=(x/2)) {
+							arr[i][j]=arr[i-1][j]-1;
+							System.out.print("["+i+"]"+"["+j+"]"+"/"+arr[i][j]); 
+							//위쪽 보다 1감소
+						}
+					}
+					else{
+						if(j<=i){
+							arr[i][j]=arr[i][j-1]-1;
+							System.out.print("["+i+"]"+"["+j+"]"+"/"+arr[i][j]); 
+							//왼쪽 열보다 1감소
+						}
+						else if(j>=(x-i)) {
+							arr[i][j]=arr[i-1][j]+1;
+							System.out.print("["+i+"]"+"["+j+"]"+"/"+arr[i][j]); 
+							//위쪽 행보다 1 증가
+						}
+					}
+				}
+				else if(i==(x-1)) {
+					num = 3*x-2-j;
+					arr[i][j] = num;
+					System.out.print("["+i+"]"+"["+j+"]"+"/"+arr[i][j]); 
+					//마지막 행
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	public void test2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수를 입력하시오.");
+		int x= sc.nextInt();
+		int[][] arr = new int[x][x];
+		int num = 1;
+		arr[0][0]=0;
+		//행 열 전환 수 1 일경우 행증가 -1 일경우 열증가
+		int swt = 1;
+		int r = 0;
+		int c = 0;
+		
+		while(true) {
+			int cnt = 0;
+			if(swt==1) {
+				for(int i = 0; i < x; i++) {
+					arr[r][c+i]++;
+					cnt++;
+					if(cnt == x-1) {
+						swt *= -1;
+						break;
+					}
+				}
+			}
+			else {
+				for(int i = 0; i < x; i++) {
+					arr[r+i][c]++;
+					if(c+i == x-1) {
+						swt *= -1;
+						break;
+					}
+				}
+			}
+			break;
+		}
+		for(int i = 0; i < x; i++) {
+			for(int j = 0; j < x; j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ 201215_HW_NestedLoop_2DimetionArray/src/com/kh/test/tow/dimention/Sample.java
@@ -0,0 +1,84 @@
+package com.kh.test.tow.dimention;
+
+public class Sample {
+	public static void main(String[] args) {
+		Sample s =new Sample();
+		s.test2();
+
+	}
+//	1. 3행짜리5열 2차원배열 선언 및 생성
+//	2. 1~100사이의 임의의 정수를 모든 방에 기록함
+//	3. 각 행별 열의 합계가 5의 배수인 행열값만 출력함
+//  이놈은 5만 찾아서 출력했네요....
+	public void test() {
+		int cnt =0;
+		int[][] arr = new int[3][5];
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = (int)(Math.random()*100)+1;
+			}
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+					System.out.print("["+arr[i][j]+"]");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("==============================");
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				if(arr[i][j]%5==0){
+					System.out.print("["+arr[i][j]+"]");
+					cnt ++;
+				}
+				else {
+					System.out.print("[  ]");
+				}
+			}
+			System.out.println();
+		}
+		
+		System.out.println("5의 배수의 갯수:"+cnt);
+	}
+
+//	이놈이 의도한대로
+	public void test2() {
+		int[][] arr = new int[3][5];
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = (int)(Math.random()*100)+1;
+			}
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr[i].length; j++) {
+					System.out.print("["+arr[i][j]+"]");
+			}
+			System.out.println();
+		}
+		System.out.println("==============================");
+		
+		for(int i = 0; i < arr.length; i++) {
+			int sum = 0;
+			for(int j = 0; j < arr[i].length; j++) {
+				sum += arr[i][j];
+			}
+			if(sum%5==0) {
+				for(int j = 0; j < arr[i].length; j++) {
+					System.out.print("["+arr[i][j]+"]");
+				}
+				System.out.print(":"+sum);
+			}
+			else {
+				for(int j = 0; j < arr[i].length; j++) {
+					System.out.print("[  ]");
+				}
+				System.out.print(":"+sum);
+			}
+			System.out.println();
+		}
+	}
+}
